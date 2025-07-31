6c044ab41516d54a3e2e890af09424a3d463683e
Ruben Da Silva <ruben.dasilva@propaysystems.com>
2025-07-31 13:30:21 +0200

    Used example.json to test for Correct Member Types

--- cypress/e2e/Petra/Utility Checks/roleCardCheck.cy.js
+++ cypress/e2e/Petra/Utility Checks/roleCardCheck.cy.js
@@ -11,7 +11,7 @@ describe('Correct Member Type Check', () => {
   });
 
   it('searches for an active member (active) & checks the role card', () => {
-    cy.fixture('activeMemberType').then((members) => {
+    cy.fixture('example').then((members) => {
       const member = members[Math.floor(Math.random() * members.length)];
 
       const searchableFields = [
@@ -45,12 +45,29 @@ describe('Correct Member Type Check', () => {
       cy.wait(1000);
 
       cy.contains('a', 'Membership Details').should('be.visible').click();
+      cy.wait(1000);
 
-      // todo: Add a check for the role card
+      cy.reload();
+      cy.contains('p', 'Update members role.').should('be.visible');
+      
+      // add if block to check if the role card is visible based on the text "For role selection, please assign a branch to the member"
+      cy.get('div.text-gray-400.col-span-4')
+        .then(($el) => {
+          const text = $el.text().trim();
+
+          if (text.includes('For role selection, please assign a branch to the member')) {
+            // If the warning is present, click the Edit button
+                cy.contains('button', 'Edit').should('be.visible').click();
+          } else {
+            // If the warning is NOT present, check that the TAK | BRANCH label exists
+            cy.contains('label.font-bold', 'TAK | BRANCH')
+              .should('exist');
+          }
+        });
     });
   });
 
-  it.skip('checks if the role card does not appear for a Member', () => {
+  it.skip('searches for any member & checks the role card', () => {
     cy.fixture('memberssearchtable').then((members) => {
     const member = members[Math.floor(Math.random() * members.length)];
 
