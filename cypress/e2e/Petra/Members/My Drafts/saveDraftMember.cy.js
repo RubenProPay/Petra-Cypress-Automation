5d8f3eb08a7ee11b55159bf57b22e5078bf34259
Ruben Da Silva <ruben.dasilva@propaysystems.com>
2025-09-05 10:30:12 +0200

    save draft member

--- cypress/e2e/Petra/Members/My Drafts/saveDraftMember.cy.js
+++ cypress/e2e/Petra/Members/My Drafts/saveDraftMember.cy.js
@@ -39,4 +39,56 @@ describe('Navigate to Create Member Page & Save a Draft Member', () => {
     cy.searchableDropdownMC('province_id', 'Province', memberFixture.address.province);
     cy.get('button:visible').filter((i, el) => Cypress.$(el).text().trim() === 'Save Draft').first().scrollIntoView().should('be.visible').click({ force: true });
   });
+
+  it('can confirms the draft member exists in My Drafts', () => {
+    cy.visit('/members/my-drafts');
+    cy.wait(2000);
+    cy.get('input[type="search"]').should('be.visible').type(memberFixture.id_number, { delay: 50 });
+    cy.get('body').type('{enter}');
+    cy.wait(3000);
+    
+    cy.contains('td', memberFixture.id_number)
+      .parents('tr')
+      .within(() => {
+        cy.get('.fi-icon-btn').click({ force: true }); 
+        cy.contains('span', 'Edit').click({ force: true });
+      });
+
+    cy.wait(750);
+    cy.contains('a', 'Personal Details').should('be.visible');
+    cy.get('input[id="id_number"]').should('have.value', memberFixture.id_number);
+    cy.get('input[id="firstname"]').should('have.value', memberFixture.firstname);
+    cy.get('input[id="surname"]').should('have.value', memberFixture.surname);
+    cy.get('input[id="cell"]').should('have.value', memberFixture.cellphone);
+    cy.get('input[id="email"]').should('have.value', memberFixture.email);
+    cy.wait(500);
+  });
+
+  it('can find and submit the draft member for approval', () => {
+    cy.visit('/members/my-drafts');
+    cy.wait(2000);
+    cy.get('input[type="search"]').should('be.visible').type(memberFixture.id_number, { delay: 50 });
+    cy.get('body').type('{enter}');
+    cy.wait(3000);
+    
+    cy.contains('td', memberFixture.id_number)
+      .parents('tr')
+      .within(() => {
+        cy.get('.fi-icon-btn').click({ force: true }); 
+        cy.contains('span', 'Edit').click({ force: true });
+      });
+
+    cy.contains('a', 'Personal Details').should('be.visible');
+    cy.get('input[id="id_number"]').should('have.value', memberFixture.id_number);
+    cy.get('input[id="firstname"]').should('have.value', memberFixture.firstname);
+    cy.get('input[id="surname"]').should('have.value', memberFixture.surname);
+    cy.get('input[id="cell"]').should('have.value', memberFixture.cellphone);
+    cy.get('input[id="email"]').should('have.value', memberFixture.email);
+    cy.wait(500);
+
+    cy.get('button:visible').filter((i, el) => Cypress.$(el).text().trim() === 'Submit for Approval').first().scrollIntoView().should('be.visible').click({ force: true });
+    cy.wait(500);
+    cy.get('button:visible').filter((i, el) => Cypress.$(el).text().trim() === 'Yes, I confirm').first().should('be.visible').click({ force: true });
+    cy.wait(500);
+  });
 });
